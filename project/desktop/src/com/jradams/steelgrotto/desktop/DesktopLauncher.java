d2dc013b70d8ea7b40d78cef661373182116a349
JRAdams97 <justinadams7991@gmail.com>
2018-12-08 18:11:17 +1100

    Default LibGDX project creation through setup utility

---
+++ project/desktop/src/com/jradams/steelgrotto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jradams.steelgrotto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jradams.steelgrotto.SteelGrotto;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SteelGrotto(), config);
+	}
+}
